0df36a7c40e9e48a7b1b18d23feab6392a930098
cloverspark <alexbrenna1999@hotmail.com>
2021-08-31 08:51:01 -0700

    created Product model

---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class Product
+    {
+        /// <summary>
+        /// A salable Product 
+        /// </summary>
+        public class Product
+        {
+            [Key]//primary key
+            public int ProductID { get; set; }
+            /// <summary>
+            /// Product name
+            /// </summary>
+            public string Title { get; set; }
+            /// <summary>
+            /// Product retail price
+            /// </summary>
+            public double Price { get; set; }
+            /// <summary>
+            /// type of product
+            /// </summary>
+            public string Catagory { get; set; }
+        }
+}

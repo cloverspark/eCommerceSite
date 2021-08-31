1eed00fac6e35e425922bf6985c78501888f7c13
cloverspark <alexbrenna1999@hotmail.com>
2021-08-31 12:07:11 -0700

    finished pagination and Added page number navigation to catalog page

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -21,18 +21,32 @@ namespace eCommerceSite.Controllers
         }
 
         /// <summary>
-        /// Displays a view that lists all products
+        /// Displays a view that lists a page of products
         /// </summary>
 
-        public async Task<IActionResult> Index()
-
+        public async Task<IActionResult> Index(int? id)
         {
+            // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-coalescing-operator
+            int pageNum = id ?? 1;
+            const int PageSize = 3;
+            ViewData["CurrentPage"] = pageNum;
+
+            int numProducts = await (from p in _context.Products
+                                     select p).CountAsync();
+
+            int totalPages = (int)Math.Ceiling((double)numProducts / PageSize);
+
+            ViewData["MaxPage"] = totalPages;
             // Get all products from database
             // List<Product> products = _context.Products.ToList();
             List<Product> products =
 
-                 await(from p in _context.Products
-                       select p).ToListAsync();
+                await (from p in _context.Products
+                       orderby p.Title ascending
+                       select p)
+                        .Skip(PageSize * (pageNum - 1)) // Skip() must be before Take()
+                        .Take(PageSize)
+                        .ToListAsync();
 
 
 

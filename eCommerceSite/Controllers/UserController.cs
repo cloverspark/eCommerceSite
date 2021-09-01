b44eb7d431df2420eaba6925449f21875ba04e78
cloverspark <80292719+cloverspark@users.noreply.github.com>
2021-08-31 17:20:43 -0700

    Issue#21 allow user to log in (#24)
    * fished Issue 20 :Created a UserAccount model, Added UserAccount to DB using code first migration, Create Register view model, Added a register page and more validation, Add registered user into DB
    
    * login finished
    close #21

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
-using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace eCommerceSite.Controllers
 {
     public class UserController : Controller
@@ -46,5 +48,51 @@ namespace eCommerceSite.Controllers
 
             return View(reg);
         }
+        public IActionResult Login()
+        {
+            // Check if user already logged in
+            if (HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //UserAccount account =
+            //    await (from u in _context.UserAccounts
+            //     where (u.Username == model.UsernameOrEmail
+            //         || u.Email == model.UsernameOrEmail)
+            //         && u.Password == model.Password
+            //     select u).SingleOrDefaultAsync();
+            UserAccount account = await _context.UserAccounts
+                    .Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
+                                        userAcc.Email == model.UsernameOrEmail) &&
+                                        userAcc.Password == model.Password)
+                    .SingleOrDefaultAsync();
+
+            if (account == null)
+            {
+                // Custom error message
+                ModelState.AddModelError(string.Empty, "Credentials were not found");
+
+                return View(model);
+            }
+
+            // Log user into website
+            HttpContext.Session.SetInt32("UserId", account.UserId);
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -49,5 +49,15 @@ namespace eCommerceSite.Models
 
         [DataType(DataType.Date)] // Time is ignored
         public DateTime? DateOfBirth { get; set; }
+    }
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username or Email")]
+        public string UsernameOrEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
